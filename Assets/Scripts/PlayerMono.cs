982d9565fe0370cd6df3330ab24901af37aebff8
Iuenot <134132767+LukyanMy@users.noreply.github.com>
2026-01-08 23:05:59 +0000

    trying stuff out
    also playing with the settings

--- Assets/Scripts/PlayerMono.cs
+++ Assets/Scripts/PlayerMono.cs
@@ -1,19 +1,29 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMono : MonoBehaviour
 {
+    private Keyboard keyboard = Keyboard.current;
+    Rigidbody rb;
+    public int health = 100;
+    public float armor = 1;
+    const float speed = 10f;
+    public int speedMultiplier = 1;
+
+
     void Start()
     {
-        int health = 100;
-        int armor = 1;
-        const int speed = 10;
-        int speedMultiplier = 1;
         Debug.Log("Player initialized with health: " + health + " and armor: " + armor * 100);
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) Destroy(this.gameObject);
+        Vector3 playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetKeyDown("space") ? 0: 1);
+        transform.position = transform.position + playerInput.normalized * speed * Time.deltaTime;
     }
+
 }
